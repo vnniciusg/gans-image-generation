a96b7d589a734061ecc2e78e184ddf4ee4a436f5
vnn <vinnicius.santos@fagoceducacional.onmicrosoft.com>
2025-04-12 06:36:32 -0300

    feat: add the util function for plot and the load model

---
+++ main.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+
+import torch
+
+from src.config import Config
+from src.model.generative import Generator
+from src.utils import generate_and_show_images
+
+
+def load_model(checkpoint_path: Path, config: Config, device: torch.device) -> Generator:
+    """
+    Load the saved model for image generation.
+
+    Args:
+        checkpoint_path (Path): the path of the checkpoint file .pth
+
+    Returns:
+        Generator: Generator model loaded and ready for inference
+    """
+    generator = Generator(config.NOISE_DIM).to(device)
+
+    checkpoint = torch.load(checkpoint_path, map_location=device)
+
+    generator.load_state_dict(checkpoint["generator"])
+    generator.eval()
+
+    return generator
+
+
+def main():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    config = Config()
+    generator = load_model(Path("checkpoints/20250412_061530/best_model_20250412_061530.pth"), config, device)
+    generate_and_show_images(generator, device=device, noise_dim=config.NOISE_DIM)
+
+if __name__ == '__main__':
+    main()
--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -6,20 +6,25 @@ import torch
 import torchvision
 from matplotlib import pyplot as plt
 
+from src.model.generative import Generator
 
-def generate_and_show_images(generator: torch.nn.Module, num_images: int = 10, noise_dim: int = 100) -> None:
+
+def generate_and_show_images(
+    generator: Generator, device: torch.device, num_images: int = 10, noise_dim: int = 100
+) -> None:
     """
     This function generates images using the provided generator model and displays them.
 
     Args:
-        generator (torch.nn.Module): The generator model used to create images.
+        generator (Generator): The generator model used to create images.
         num_images (int): The number of images to generate.
         noise_dim (int): The dimension of the noise vector used for generation.
     """
-    noise = torch.rand(num_images, noise_dim)
+    generator.to(device)
+    noise = torch.rand(num_images, noise_dim).to(device)
     generated_images = generator(noise)
 
-    generated_images = generated_images.view(num_images, 1, 28, 28)
+    generated_images = generated_images.view(num_images, 1, 28, 28).to(device)
     grid = torchvision.utils.make_grid(generated_images, nrow=5, normalize=True)
     plt.figure(figsize=(10, 5))
     plt.imshow(grid.permute(1, 2, 0).detach().cpu().numpy())
